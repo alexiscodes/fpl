5f78f3a2a9dbf77dd5bf64b279e374d063ce5ae4
Alexis Becker <alexis.becker@transferwise.com>
2018-10-06 20:24:36 +0100

    adding postgres tables

--- fpl_website.py
+++ fpl_website.py
@@ -1,17 +1,37 @@
-from flask import Flask
 
 #takes Jinja template as input and produces pure HTML as output
-from flask import render_template
+
+from flask import Flask, render_template, url_for
+
+#https://dev.to/paultopia/the-easiest-possible-way-to-throw-a-webapp-online-flask--heroku--postgres-185o
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+import requests
+import json
+import pandas as pd
+from flask_heroku import Heroku
+import os
 
 #Get formulas
-from database.fpl_methods import get_all_data
+from .database.fpl_methods import get_all_data
 
 app = Flask(__name__)
 
+#Conncet db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+#Homepage - show table with all players from db
 @app.route("/")
 def get_news():
     players = get_all_data()  
     return render_template('tables.html',tables=[players.to_html()], titles = ['players'], classes='players')
+
+
 
 if __name__ == '__main__':
   app.run(port=5000, debug=True)
---
+++ models.py
@@ -0,0 +1,19 @@
+from .fpl_website import db
+from sqlalchemy.dialects.postgresql import JSON
+
+#https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+class Result(db.Model):
+    __tablename__ = 'positions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    element_type = db.Column(db.String(), index=True)
+    singular_name = db.Column(db.String())
+    singular_name_short = db.Column(db.String())
+
+    def __init__(self, element_type, singular_name, singular_name_short):
+        self.element_type = element_type
+        self.singular_name = singular_name
+        self.singular_name_short = singular_name_short
+
+    def __repr__(self):
+        return '<Name {}>'.format(self.singular_name)
